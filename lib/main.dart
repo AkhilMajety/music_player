b65be2185806a320065297ad55fd1676292ec7d7
Akhil <babloo5300@gmail.com>
2021-01-12 12:08:17 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/colors.dart';
+import 'package:music_player/playercontrols.dart';
+
+import 'albumart.dart';
+import 'navbar.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(fontFamily: 'Circular'),
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double sliderValue = 2;
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Navbar(),
+          Container(
+            height: height / 2.5,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return AlbumArt();
+              },
+              itemCount: 3,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Text('Gidget',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: darkPrimaryColor,
+              )),
+          Text('The free Nationals',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: darkPrimaryColor,
+              )),
+          SliderTheme(
+            data: SliderThemeData(  
+              trackHeight: 5,
+              thumbShape: RoundSliderThumbShape(enabledThumbRadius: 5)
+            ),
+                      child: Slider(
+              value: sliderValue,
+              activeColor: darkPrimaryColor,
+              inactiveColor: darkPrimaryColor.withOpacity(0.3),
+              onChanged: (value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+              min: 0,
+              max: 20,
+            ),
+          ),
+
+          PlayerControls(),
+          SizedBox(height: 40,)
+        ],
+      ),
+    );
+  }
+}
